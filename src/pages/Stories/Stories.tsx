bf1238a1029300c8223493a2b0b9b81f15420f86
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2026-07-06 00:47:19 +0530

    remove story heading

--- src/pages/Stories/Stories.tsx
+++ src/pages/Stories/Stories.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Container, Typography } from "@mui/material";
-import StoryList from "../../components/Story/StoryList"; 
+import StoryList from "../../components/Story/StoryList";
 import StoryViewer from "../../components/Story/StoryViewer";
 import { stories } from "../../data/story.data"
 import type { Story } from "../../data/story.data";
@@ -9,14 +9,8 @@ export default function Stories() {
   const [selected, setSelected] = useState<Story>();
 
   return (
-    <Container maxWidth="md">
-
-      <Typography
-        variant="h5"
-        sx={{ mt: 3, mb: 2 }}
-      >
-        Stories
-      </Typography>
+    <Container maxWidth="md"
+      sx={{ mt: 2, mb: 2 }}>
 
       <StoryList
         stories={stories}
