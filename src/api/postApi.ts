d715b51cec249930e00ac3be3c3652266ea6bd45
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2026-09-01 16:49:51 +0530

    remove redundance code

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -2,8 +2,7 @@ import type { Post } from "../types/post.types";
 import api from "./axios"
 
 export const getAllPostByUserId = async (id: string): Promise<Post[]> => {
-    console.log("User id "+id);
     const response = await api.get<Post[]>(`post/api/posts/user/${id}`);
-    console.log("Respose Post data ",response.data);
     return response.data;
 }
+
