3f72a854f95da261e57d33f8a32d50094ff05562
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2026-09-10 05:06:17 +0530

    stories list add'

--- src/components/Post/PostList.tsx
+++ src/components/Post/PostList.tsx
@@ -2,13 +2,14 @@ import { Stack } from "@mui/material";
 import PostComponent from "./PostComponent";
 import type { Feed } from "../../types/feed.types";
 import PageSkeleton from "../Skeleton/PageSkeleton";
+import Stories from "../../pages/Stories/Stories";
 
 interface Props {
     feeds: Feed[] | undefined,
 }
 
 export default function PostList({ feeds }: Props) {
-    if (!feeds) return <PageSkeleton/> // or null, or a skeleton
+    if (!feeds) return <PageSkeleton /> // or null, or a skeleton
 
     return (
         <Stack
@@ -24,7 +25,7 @@ export default function PostList({ feeds }: Props) {
             }}
 
         >
-
+            <Stories />
             {feeds.map((post, index) => (
                 <PostComponent
                     key={index}
